07de95e4aa708bfc3e52ce817c7c766b3fe601f1
Praveen <arumallapraveen03@gmail.com>
2022-09-19 19:35:22 +0530

    Practical-1

---
+++ src/main/kotlin/practical1_1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val intval: Int = 20
+    val floatval: Double = 2.0
+    val charval: Char = 'p'
+    val stringval: String = "praveen"
+    val boolval: Boolean = true
+    val doubleval: Double = 95.7
+    val longval: Long = 7799643281
+    val shortval: Short = 1
+    val byteval: Byte = 64
+    println("Interger Value : " + intval)
+    println("Float Value : " + floatval)
+    println("Character Value : " + charval)
+    println("String Value : " + stringval)
+    println("Boolean Value : " + boolval)
+    println("Double Value : " + doubleval)
+    println("Long Value : " + longval)
+    println("Short Value : " + shortval)
+    println("Byte Value : " + byteval)
+}
